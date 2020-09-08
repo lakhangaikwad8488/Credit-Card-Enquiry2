818da7f99708d23b693a810d64dcb0cf2fc95355
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-09-08 15:43:18 +0530

    Add files via upload

---
+++ Credit Card Enquiry/src/test/java/yaksha/CustomerMainTest.java
@@ -0,0 +1,25 @@
+package test.java.yaksha;
+import main.java.yaksha.CustomerMain;
+import main.java.yaksha.TestUtils;
+import main.java.yaksha.Customer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerMainTest {
+
+	@Test
+	void testDisplay() throws IOException {
+		 
+		long mobile=989898986;
+		Customer c=new Customer(mobile, "Narayan", "Pune", "Wipro","Brazil", 20000);
+		//assertEquals(true, c.display());
+		TestUtils.yakshaAssert(TestUtils.currentTest(),c.display()==true==true?"true":"false",TestUtils.businessTestFile);
+
+		
+ 	}
+
+}
